6a777c2ae7817d9031f8101aeb84270b1fe27a82
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-05-05 10:28:29 -0700

    header component with add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,11 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
-  template: `
-    <main>
-      <header class="brand-name">
-        <img
-          class="brand-logo"
-          src="/assets/logo.svg"
-          alt="logo"
-          aria-hidden="true"
-        />
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
+  imports: [HeaderComponent],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'homes';
-}
+export class AppComponent {}
