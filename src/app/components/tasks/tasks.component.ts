12e20d263e9cd7f255a7fb10beb85c1e364925e0
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-05-09 07:29:50 -0700

    print tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { TASKS } from '../../mock-tasks';
+import { Task } from '../../mock-tasks';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-
+  tasks: Task[] = TASKS;
 }
